cb87bd134da2eea8fee5180c7f3608bda7ef1692
Ivano Gavran Stojanovski <Ivano_GavranStojanovski@sluz.ch>
2025-11-21 12:37:18 +0100

    Go-1-ex-1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+var firstName string = "Ivano"
+var lastName string = "Gavran Stojanovski"
+var dayOfBirth int = 1
+var monthOfBirth int = 12
+var yearOfBirth int = 2007
+var numberOfSiblings int = 1
+var heightInMeters float32 = 1.70
+var zodiacSign byte = 'S'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
