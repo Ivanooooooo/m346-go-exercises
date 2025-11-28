18f284d9c17e7d42038afd314e75178ba59e2ac4
Ivano Gavran Stojanovski <Ivano_GavranStojanovski@sluz.ch>
2025-11-28 12:29:59 +0100

    go-2-ex-3

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,11 +4,19 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
+	modules := map[uint16]string{
+		104: "Ein super cooles Modul",
+		117: "Netzwerkmodul",
+		346: "Cloudmodul",
+	}
 
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
 
+	delete(modules, 104)
+	modules[164] = "SQL-Modul"
+	modules[117] = "Fortgeschrittenes Netzwerkmodul"
 	// TODO: delete one
 	// TODO: add one
 	// TODO: replace one
