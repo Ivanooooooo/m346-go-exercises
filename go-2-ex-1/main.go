881e846fa1dabe68bb543c3d10646302448b5a7b
Ivano Gavran Stojanovski <Ivano_GavranStojanovski@sluz.ch>
2025-11-28 12:13:33 +0100

    go-2-ex-1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirtstName string
+	LastName   string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	DayOfBirth   byte
+	MonthOfBirth byte
+	YearOfBirth  int16
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	ProfileName      FullName
+	BirthDateInfo    BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,22 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		ProfileName: FullName{
+			FirtstName: "Ivano",
+			LastName:   "Gavran Stojanovski",
+		},
+		BirthDateInfo: BirthDate{
+			DayOfBirth:   1,
+			MonthOfBirth: 12,
+			YearOfBirth:  2007,
+		},
+		NumberOfSiblings: 1,   // TODO: adjust
+		ZodiacSign:       'S', // TODO: adjust
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
