2e0282bda9e78a5f2ef71300b6768958711037c5
Ivano Gavran Stojanovski <Ivano_GavranStojanovski@sluz.ch>
2025-12-05 12:30:19 +0100

    go-3-ex-4

--- go-3-ex-4/main.go
+++ go-3-ex-4/main.go
@@ -24,7 +24,11 @@ func main() {
 	ranks := []rune{Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
 
 	// TODO: Loop over suits and ranks to output all combinations.
+	for _, suits := range suits {
+		for _, ranks := range ranks {
+			fmt.Printf("%c%c ", ranks, suits)
+		}
+		fmt.Println(suits, ranks)
 
-	// TODO: delete this line afterwards
-	fmt.Println(suits, ranks)
+	}
 }
