cbed8a4e2a487a2d1abb1185472252ecb7d8419b
Ivano Gavran Stojanovski <Ivano_GavranStojanovski@sluz.ch>
2025-12-12 11:57:45 +0100

    go-4-ex-1

--- go-4-ex-1/main.go
+++ go-4-ex-1/main.go
@@ -1,7 +1,22 @@
 package main
 
-// TODO: implement the function computeGrade
+func computeGrade(score int) string {
+	if score >= 90 {
+		return "A"
+	} else if score >= 80 {
+		return "B"
+	} else if score >= 70 {
+		return "C"
+	} else if score >= 60 {
+		return "D"
+	} else if score >= 50 {
+		return "E"
+	} else {
+		return "Keine F"
+	}
+}
 
 func main() {
-	// TODO: call the function computeGrade
+	grade := computeGrade(89)
+	println(grade)
 }
