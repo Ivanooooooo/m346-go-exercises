69451eb9f955d0f1289c8c4cbe12a4cfe205e7ba
Ivano Gavran Stojanovski <Ivano_GavranStojanovski@sluz.ch>
2025-12-12 12:21:47 +0100

    go-4-ex-3

--- go-4-ex-3/main.go
+++ go-4-ex-3/main.go
@@ -1,7 +1,14 @@
 package main
 
-// TODO: implement the function computeQuadraticFormula
+import "math"
+
+func computeQuadraticFormula(a float64, b float64, c float64) (float64, float64) {
+	positive := (-b + math.Sqrt(math.Pow(b, 2)-4*a*c)) / (2 * a)
+	negative := (-b - math.Sqrt(math.Pow(b, 2)-4*a*c)) / (2 * a)
+	return positive, negative
+}
 
 func main() {
-	// TODO: call the function computeQuadraticFormula
+	positiveLösung, negativeLösung := computeQuadraticFormula(1, -3, 2)
+	println(positiveLösung, "+-", negativeLösung)
 }
